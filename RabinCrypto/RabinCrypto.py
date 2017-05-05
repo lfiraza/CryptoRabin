34e077bf99253e221cdcfd46489511483339876f
Lukasz Firaza <lukasz.firaza@gmail.com>
2017-05-05 22:00:23 +0200

    refactoring

--- RabinCrypto/RabinCrypto.py
+++ RabinCrypto/RabinCrypto.py
@@ -4,12 +4,13 @@ import sys
 private = [67317488495423799828622848507978495932353996728161028786981841987819294308223,
            88916115075035204878391459198246505521471227845052526477778284545394011218787]
 
-#private = [167,
- #          151]
+private = [7,11]
 
 #calc n
 public = private[0]*private[1]
+print("Klucz publiczny")
 print(public)
+print("----------")
 
 def egcd(x, y):
     xp, xq = 1, 0
@@ -19,66 +20,40 @@ def egcd(x, y):
         t, tp, tq = x, xp, xq
         x, xp, xq = y, yp, yq
         y, yp, yq = t - q * x, tp - q * xp, tq - q * xq
-    return (x, xp, xq)
+    return (xp, xq)
 
 
-msg = "Testowa wiadomosc"
-
-msg_dec = int.from_bytes(msg.encode(), sys.byteorder)
-msg_dec = 2216014906335081603549288532914445221681335498308087915761920903129755171763383337516126256589945957688750848529770360764804066357509839523612945675889149
+msg = 45
 
 print("Wiadomosc")
 print(msg_dec)
 print("---------")
 
-encrypted = pow(msg_dec, 2, public)
+encrypted = pow(msg, 2, public)
 
 print("Szyfrogram")
 print(encrypted)
 print("---------")
 
 #chinskie twierdzenie o resztach
-'''
-_, a, b = egcd(private[0], private[1])
-
-r = pow(encrypted, int((private[0]+1)/4), private[0])
-r1 = (encrypted**int(private[0]+1/4)) % private[0]
-s = pow(encrypted, int((private[1]+1)/4), private[1])
-x = (a*private[0]*s + b*private[1]*r) % public
-y = (a*private[0]*s - b*private[1]*r) % public
-
-print(r)
-print(r1)
-
-dM1 = x
-dM2 = y
-dM3 = -x % public
-dM4 = -y % public
-
-
-print(dM1)
-print(dM2)
-print(dM3)
-print(dM4)
-'''
 
 m1 = pow(encrypted, int((private[0]+1)/4), private[0])
 m2 = private[0] - pow(encrypted, int((private[0]+1)/4), private[0])
 m3 = pow(encrypted, int((private[1]+1)/4), private[1])
 m4 = private[1] - pow(encrypted, int((private[1]+1)/4), private[1])
 
-_, aE, bE = egcd(private[0], private[1])
+modInv = egcd(private[0], private[1])
 
-a = private[1]*bE
-b = private[0]*aE
+a = private[1]*modInv[1]
+b = private[0]*modInv[0]
 
-dM1 = pow((a*m1 + b*m3), 1, public)
-dM2 = pow((a*m1 + b*m4), 1, public)
-dM3 = pow((a*m2 + b*m3), 1, public)
-dM4 = pow((a*m2 + b*m4), 1, public)
+M1 = pow((a*m1 + b*m3), 1, public)
+M2 = pow((a*m1 + b*m4), 1, public)
+M3 = pow((a*m2 + b*m3), 1, public)
+M4 = pow((a*m2 + b*m4), 1, public)
 
-print(dM1)
-print(dM2)
-print(dM3)
-print(dM4)
+print(M1)
+print(M2)
+print(M3)
+print(M4)
 
